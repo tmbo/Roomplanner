0e89e344c8274e49dd5c3e7d2d78655cde12c288
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2013-01-05 12:44:32 +0100

    working on gui

--- src/apps/GUIApp.cc
+++ src/apps/GUIApp.cc
@@ -7,10 +7,12 @@
 #include "widgets/FlickArea.h"
 #include "widgets/ImageWidget.h"
 #include "widgets/PageIndicatorWidget.h"
+#include "widgets/ClickableWidget.h"
+#include <QSignalMapper.h>
+#include <stdio.h>
 
 namespace ipn
 {
-
 	GUIApp::GUIApp(QWidget *parent) : App(parent)
 	{
         m_back = new BackgroundWidget(this);
@@ -26,24 +28,44 @@ namespace ipn
         m_flickArea->move(0, 50);
         m_flickArea->resize(240, 150);
 
-        m_image[0] = new ImageWidget(m_flickArea);
-        m_image[0]->setImage(":/img/backgrounds/map.png");
+        m_clickable0 = new ClickableWidget(m_flickArea);
+        m_clickable0->resize(240, 200);
+        m_clickable0->move(0,0);
 
-        m_image[1] = new ImageWidget(m_flickArea);
-        m_image[1]->setImage(":/img/backgrounds/map.png");
-/*
-		m_scrollText = new TextWidget(m_flickArea);
-		m_scrollText->setText("Scroll me! Lorem ipsum dolor sit amet adscipsing consectetur ...");
-		m_scrollText->move(128, 0);
-		m_scrollText->resize(768, 64);
-		m_scrollText->setColor(Qt::white);
-		m_scrollText->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
-*/
+        m_image0 = new ImageWidget(m_clickable0);
+        m_image0->setImage(":/assets/images/sater.png");
+        m_image0->resize(240, 200);
+
+        m_clickable1 = new ClickableWidget(m_flickArea);
+        m_clickable1->resize(240, 200);
+        m_clickable1->move(240,0);
+
+        m_image1 = new ImageWidget(m_clickable1);
+        m_image1->setImage(":/assets/images/karlstad.png");
+        m_image1->resize(240, 200);
+
+        m_clickable2 = new ClickableWidget(m_flickArea);
+        m_clickable2->resize(240, 200);
+        m_clickable2->move(240 * 2,0);
+
+        m_image2 = new ImageWidget(m_clickable2);
+        m_image2->setImage(":/assets/images/ektorp.png");
+        m_image2->resize(240, 200);
+
 		m_pageIndicator = new PageIndicatorWidget(this);
-		m_pageIndicator->setNumberOfSegments(6);
+        m_pageIndicator->setNumberOfSegments(3);
 		m_pageIndicator->move(120 - m_pageIndicator->width() / 2, 224);
 
-		connect(m_flickArea, SIGNAL(moved()), this, SLOT(updatePageIndicator()));
+        /*QSignalMapper *signalMapper = new QSignalMapper(this);
+        signalMapper->setMapping(m_clickable0, 0);
+        signalMapper->setMapping(m_clickable1, 1);
+        signalMapper->setMapping(m_clickable2, 2);*/
+
+        //connect(m_flickArea, SIGNAL(moved()), this, SLOT(updatePageIndicator()));
+        //connect(m_clickable0, SIGNAL(released()), signalMapper, SLOT(map()));
+        //connect(m_clickable1, SIGNAL(released()), signalMapper, SLOT(map()));
+        //connect(m_clickable2, SIGNAL(released()), signalMapper, SLOT(map()));
+        //connect(signalMapper, SIGNAL(mapped(int)), this, SLOT(furnitureSelected(int)));
 	}
 
 	TitleBarWidget *GUIApp::titleBar()
@@ -55,5 +77,10 @@ namespace ipn
 	{
 		m_pageIndicator->setPosition(m_flickArea->relativeScrollPosition().x());
 	}
+
+    void GUIApp::furnitureSelected(int idx)
+    {
+       // printf("Idx ");
+    }
 
 } // namespace ipn
